0671fc516e9853dbb2ffc6aff92d1e669ed61a89
Ngoc Doan <ngocdoan241@gmail.com>
2020-12-15 20:53:57 -0700

    first changes

---
+++ app.js
@@ -0,0 +1,102 @@
+// console.log ("hello world");
+
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+
+// for(let i=0; i <10; i++) {
+//     document.write('Hello World'+'<br/>');
+// }
+
+
+
+function assignGrade(score) {
+    if (score > 90) {
+        return 'A';
+    }
+    else if (score > 80) {
+        return 'B';
+    }
+
+    else if (score > 70) {
+        return 'C'; 
+    }
+
+    else if (score > 60) {
+        return 'D';
+    }
+
+    else {
+        return 'F';
+    }
+}
+
+document.write(' You got an ' + assignGrade(86) + "<br>");
+document.write(' You got an' + assignGrade(55) +"<br>");
+document.write(' You got an ' + assignGrade (77) + "<br>");
+
+
+
+// var money=3500.00;
+// var bills=3500.00;
+
+// if (money > bills || (money < 500.00)) {
+//     document.write ("You are Covered");
+// }
+
+// else if (money=== bills) || (money <3000) {
+//     document.write("You're Covered but BARELY");
+// }
+
+// else if (money > bills) && {money < 500)) {
+
+//     document.write("You're Good!");
+
+// }
+
+// else {
+//     document.write ("uh-oh!");
+// };
+
+
+
+
+// for (let i=0; i <=20; i++) {
+// if (i % 2=== 0) {
+//     document.write(i + "is even");
+// }
+// else {
+//     document.write(i + "is odd");
+// }
+// }
+
+
+
+// for (i=0; i<10; i++) {
+//     if (i===7) {
+//         break;
+//     } }
+    
+// document.write ("The number is" + i + "<br>");
+
+
+// var x =0;
+// while(x < 5) {
+//     console.log(x);
+
+//     x=x+1;
+// };
+
+// var x= 4;
+// while (x < 5) {
+//     console.log(x);
+//     x=x + 1;
+
+// };

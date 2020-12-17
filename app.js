b702d95c7a587556598c3f324199b9f5e7fc2a42
Ngoc Doan <ngocdoan241@gmail.com>
2020-12-17 12:17:30 -0700

    code code

--- app.js
+++ app.js
@@ -1,102 +1,131 @@
-// console.log ("hello world");
-
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-// document.write ('Hello World'+'<br/>');
-
-// for(let i=0; i <10; i++) {
-//     document.write('Hello World'+'<br/>');
+// function checkAge(age) {
+//     if (age >= 18) {
+//         return ('You may enter');
+//     }
+//     else if (age <=18) {
+//         return ("Not Old Enough")
 // }
 
+// let age = prompt ("How old are you?", 18);
 
+// if (checking(age)) {
+//     alert ('You may enter' );
+// }
+// else { 
+//     alert ('Denied');
+// };
 
-function assignGrade(score) {
-    if (score > 90) {
-        return 'A';
-    }
-    else if (score > 80) {
-        return 'B';
-    }
-
-    else if (score > 70) {
-        return 'C'; 
-    }
-
-    else if (score > 60) {
-        return 'D';
-    }
-
-    else {
-        return 'F';
-    }
-}
-
-document.write(' You got an ' + assignGrade(86) + "<br>");
-document.write(' You got an' + assignGrade(55) +"<br>");
-document.write(' You got an ' + assignGrade (77) + "<br>");
-
-
-
-// var money=3500.00;
-// var bills=3500.00;
-
-// if (money > bills || (money < 500.00)) {
-//     document.write ("You are Covered");
 // }
 
-// else if (money=== bills) || (money <3000) {
-//     document.write("You're Covered but BARELY");
-// }
+// var checkAge = prompt("How old are you?", 18);
 
-// else if (money > bills) && {money < 500)) {
-
-//     document.write("You're Good!");
-
+// if (check(age >=18)) {
+//     var checkAge = prompt('You may enter');
 // }
 
 // else {
-//     document.write ("uh-oh!");
+//     alert ('Denied');
+
+// }
+
+
+let age = prompt("how old are you?");
+let conf = confirm(`are you ${age} years old ?`);
+if (age >=18) {
+alert(`nice, you're ${age} years old, you may enter!`);
+} 
+
+else {
+alert(`you're ${age}. Access denied! `);
+}
+
+// if (age >=18) {
+//   txt = ("You may enter");
+
+// }
+//  else {
+//   txt = "Not old enough";
+// }
 // };
 
 
-
-
+// console.log ("hello world");
+ 
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+// document.write ('Hello World'+'<br/>');
+ 
+// for(let i=0; i <10; i++) {
+//     document.write('Hello World'+'<br/>');
+// }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// function assignGrade(score) {
+//     if (score > 90) {
+//         return 'A';
+//     }
+//     else if (score > 80) {
+//         return 'B';
+//     }
+ 
+//     else if (score > 70) {
+//         return 'C';
+//     }
+ 
+//     else if (score > 60) {
+//         return 'D';
+//     }
+ 
+//     else {
+//         return 'F';
+//     }
+// }
+ 
+// document.write(' You got an ' + assignGrade(86) + "<br>");
+// document.write(' You got an' + assignGrade(55) +"<br>");
+// document.write(' You got an ' + assignGrade (77) + "<br>");
+ 
+ 
+ 
+// var money=3500.00;
+// var bills=3500.00;
+ 
+// if (money > bills || (money < 500.00)) {
+//     document.write ("You are Covered");
+// }
+ 
+// else if (money=== bills) || (money <3000) {
+//     document.write("You're Covered but BARELY");
+// }
+ 
+// else if (money > bills) && {money < 500)) {
+ 
+//     document.write("You're Good!");
+ 
+// }
+ 
+// else {
+//     document.write ("uh-oh!");
+// };
+ 
+ 
+ 
+ 
 // for (let i=0; i <=20; i++) {
 // if (i % 2=== 0) {
 //     document.write(i + "is even");
 // }
 // else {
 //     document.write(i + "is odd");
-// }
-// }
-
-
-
-// for (i=0; i<10; i++) {
-//     if (i===7) {
-//         break;
-//     } }
-    
-// document.write ("The number is" + i + "<br>");
-
-
-// var x =0;
-// while(x < 5) {
-//     console.log(x);
-
-//     x=x+1;
-// };
-
-// var x= 4;
-// while (x < 5) {
-//     console.log(x);
-//     x=x + 1;
-
-// };
